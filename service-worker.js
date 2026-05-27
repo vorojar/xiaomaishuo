3c0c79abd4caaf15d71068dadd2b6ddddcc8f503
Vorojar <vorojar@gmail.com>
2026-05-27 16:41:06 +0800

    fix(reader): silence manual scroll pause toast

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xiaomaishuo-offline-v8-final-198225-lazyload';
+const CACHE_NAME = 'xiaomaishuo-offline-v8-final-198225-lazyload-fix1';
 const CORE_ASSETS = [
     './',
     './index.html',
