186888d0d3a3268a388e74d561e9442df7966765
Vorojar <vorojar@gmail.com>
2026-05-27 16:15:54 +0800

    fix(reader): suppress text selection on desktop double click

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xiaomaishuo-offline-v7-final-198225-autoscroll-fix1';
+const CACHE_NAME = 'xiaomaishuo-offline-v7-final-198225-autoscroll-fix2';
 const CORE_ASSETS = [
     './',
     './index.html',
