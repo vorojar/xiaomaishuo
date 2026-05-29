90116ad92a8a06b7c0ff8a03f69e5517f6e23023
Vorojar <vorojar@gmail.com>
2026-05-29 15:37:33 +0800

    feat(reader): add shareable chapter routes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xiaomaishuo-offline-v8-final-198225-lazyload-fix1';
+const CACHE_NAME = 'xiaomaishuo-offline-v9-final-198225-chapter-routes';
 const CORE_ASSETS = [
     './',
     './index.html',
